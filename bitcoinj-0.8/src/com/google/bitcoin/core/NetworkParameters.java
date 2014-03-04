0360b13244f1f76d1ca1a9894fa190a73016d7d1
AnonymousPirate <LydiaCoin@riseup.net>
2014-03-04 18:31:45 -0500

    Update NetworkParameters.java
    Updated chainparameters to match MazaCoin

--- bitcoinj-0.8/src/com/google/bitcoin/core/NetworkParameters.java
+++ bitcoinj-0.8/src/com/google/bitcoin/core/NetworkParameters.java
@@ -45,7 +45,7 @@ public class NetworkParameters implements Serializable {
     /**
      * The alert signing key originally owned by Satoshi, and now passed on to Gavin along with a few others.
      */
-    public static final byte[] SATOSHI_KEY = Hex.decode("04fc9702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
+    public static final byte[] SATOSHI_KEY = Hex.decode("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f");
 
     /** The string returned by getId() for the main, production network where people trade things. */
     public static final String ID_PRODNET = "org.bitcoin.production";
@@ -145,28 +145,25 @@ public class NetworkParameters implements Serializable {
             acceptableAddressCodes = new int[] { 0 };
             dumpedPrivateKeyHeader = 128;
             addressHeader = 0;
-            port = 8333;
+            port = 12835;
             packetMagic = 0xf9beb4d9L;
             genesisBlock.setDifficultyTarget(0x1d00ffffL);
-            genesisBlock.setTime(1231006505L);
-            genesisBlock.setNonce(2083236893);
+            genesisBlock.setTime(1390747675);
+            genesisBlock.setNonce(2091390249);
             id = ID_PRODNET;
-            subsidyDecreaseBlockCount = 210000;
+            subsidyDecreaseBlockCount = 241920;
             allowEmptyPeerChains = false;
             spendableCoinbaseDepth = 100;
             String genesisHash = genesisBlock.getHashAsString();
-            checkState(genesisHash.equals("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
+            checkState(genesisHash.equals("0x00000c7c73d8ce604178dae13f0fc6ec0be3275614366d44b1b4b5c6e238c60c"),
                     genesisHash);
 
             // This contains (at a minimum) the blocks which are not BIP30 compliant. BIP30 changed how duplicate
             // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
             // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
             // Having these here simplifies block connection logic considerably.
-            checkpoints.put(91722, new Sha256Hash("00000000000271a2dc26e7667f8419f2e15416dc6955e5a6c6cdf3f2574dd08e"));
-            checkpoints.put(91812, new Sha256Hash("00000000000af0aed4792b1acee3d966af36cf5def14935db8de83d6f9306f2f"));
-            checkpoints.put(91842, new Sha256Hash("00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec"));
-            checkpoints.put(91880, new Sha256Hash("00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721"));
-            checkpoints.put(200000, new Sha256Hash("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf"));
+            checkpoints.put(0, new Sha256Hash("0x60427defa713035a27542cb2b955b0001e01878f4828855b1fb137d3560279aa"));
+
         } else if (type == 3) {
             // Testnet3
             id = ID_TESTNET;
@@ -175,23 +172,23 @@ public class NetworkParameters implements Serializable {
             interval = INTERVAL;
             targetTimespan = TARGET_TIMESPAN;
             proofOfWorkLimit = Utils.decodeCompactBits(0x1d00ffffL);
-            port = 18333;
+            port = 11835;
             addressHeader = 111;
             acceptableAddressCodes = new int[] { 111 };
             dumpedPrivateKeyHeader = 239;
-            genesisBlock.setTime(1296688602L);
+            genesisBlock.setTime(1390747675);
             genesisBlock.setDifficultyTarget(0x1d00ffffL);
-            genesisBlock.setNonce(414098458);
+            genesisBlock.setNonce(414708675);
             allowEmptyPeerChains = true;
             spendableCoinbaseDepth = 100;
-            subsidyDecreaseBlockCount = 210000;
+            subsidyDecreaseBlockCount = 241920;
             String genesisHash = genesisBlock.getHashAsString();
-            checkState(genesisHash.equals("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"),
+            checkState(genesisHash.equals("0x000007717e2e2df52a9ff29b0771901c9c12f5cbb4914cdf0c8047b459bb21d8"),
                     genesisHash);
         } else if (type == 2) {
             id = ID_TESTNET;
             packetMagic = 0xfabfb5daL;
-            port = 18333;
+            port = 11835;
             addressHeader = 111;
             interval = INTERVAL;
             targetTimespan = TARGET_TIMESPAN;
@@ -215,7 +212,7 @@ public class NetworkParameters implements Serializable {
             genesisBlock.setTime(System.currentTimeMillis() / 1000);
             genesisBlock.setDifficultyTarget(Block.EASIEST_DIFFICULTY_TARGET);
             genesisBlock.solve();
-            port = 18333;
+            port = 11835;
             interval = 10;
             dumpedPrivateKeyHeader = 239;
             allowEmptyPeerChains = false;
@@ -236,7 +233,7 @@ public class NetworkParameters implements Serializable {
             //
             //   "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
             byte[] bytes = Hex.decode
-                    ("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
+                    ("04f09702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284");
             t.addInput(new TransactionInput(n, t, bytes));
             ByteArrayOutputStream scriptPubKeyBytes = new ByteArrayOutputStream();
             Script.writeBytes(scriptPubKeyBytes, Hex.decode
@@ -251,8 +248,8 @@ public class NetworkParameters implements Serializable {
         return genesisBlock;
     }
 
-    public static final int TARGET_TIMESPAN = 14 * 24 * 60 * 60;  // 2 weeks per difficulty cycle, on average.
-    public static final int TARGET_SPACING = 10 * 60;  // 10 minutes per block.
+    public static final int TARGET_TIMESPAN = 8 * 60;  // 2 weeks per difficulty cycle, on average.
+    public static final int TARGET_SPACING = 120;  // 10 minutes per block.
     public static final int INTERVAL = TARGET_TIMESPAN / TARGET_SPACING;
     
     /**
